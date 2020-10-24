3afcb53e51cfa0869943b4a20495477e83d90fa6
unknown <mendibass@gmail.com>
2020-10-24 05:50:35 -0700

    Updates

---
+++ precache-manifest.37052e9638e52f36335a1fabb0562812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "017497a913b3229ddcd0584148afa2d1",
+    "url": "/react-chat-client/index.html"
+  },
+  {
+    "revision": "a26eb36fb16fefc23b3d",
+    "url": "/react-chat-client/static/css/main.c4ea39eb.chunk.css"
+  },
+  {
+    "revision": "68e428eb35b3c58f2b4b",
+    "url": "/react-chat-client/static/js/2.0cd183ac.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/react-chat-client/static/js/2.0cd183ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26eb36fb16fefc23b3d",
+    "url": "/react-chat-client/static/js/main.6032c5c6.chunk.js"
+  },
+  {
+    "revision": "db01825fb8be6c136efa",
+    "url": "/react-chat-client/static/js/runtime-main.19bb2edc.js"
+  }
+]);
