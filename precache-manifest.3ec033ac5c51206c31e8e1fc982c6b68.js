96dda5aafa868f7f0938e84bfff40abf55bede78
unknown <mendibass@gmail.com>
2020-10-24 08:48:02 -0700

    Updates

--- precache-manifest.3ec033ac5c51206c31e8e1fc982c6b68.js
+++ precache-manifest.3ec033ac5c51206c31e8e1fc982c6b68.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "11d777cf52dfc175aca4e4eb8d8eccb4",
+    "revision": "ae429f8ce5dd0a0641966ef6eae95fdf",
     "url": "/react-chat-client/index.html"
   },
   {
-    "revision": "68afb879b0adac6b6b4c",
+    "revision": "a87b764df383c1e7665a",
     "url": "/react-chat-client/static/css/main.c4ea39eb.chunk.css"
   },
   {
-    "revision": "94f28c1e3a0c0c7ac012",
-    "url": "/react-chat-client/static/js/2.85c452e0.chunk.js"
+    "revision": "90532dbd1641b33f66e0",
+    "url": "/react-chat-client/static/js/2.863cca80.chunk.js"
   },
   {
     "revision": "d0550c944a9928585a31b6efc1a5d942",
-    "url": "/react-chat-client/static/js/2.85c452e0.chunk.js.LICENSE.txt"
+    "url": "/react-chat-client/static/js/2.863cca80.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "68afb879b0adac6b6b4c",
-    "url": "/react-chat-client/static/js/main.4e80829e.chunk.js"
+    "revision": "a87b764df383c1e7665a",
+    "url": "/react-chat-client/static/js/main.50f3fc5a.chunk.js"
   },
   {
     "revision": "db01825fb8be6c136efa",
