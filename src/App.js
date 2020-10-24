b2e750e90d3457ced34e72ef47b048ef337929cb
unknown <mendibass@gmail.com>
2020-10-24 08:45:54 -0700

    BrouserRouter relaced to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {HashRouter as Router, Route,Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route,Link} from 'react-router-dom';
 
 import Join from './components/Join/Join';
 import Chat from './components/Chat/Chat';
@@ -8,7 +8,7 @@ import Chat from './components/Chat/Chat';
 
 const App = ()=>(
     <div>
-        <h1>Out of the...</h1>
+      <h1>Out of the...</h1>
        <li><Link to="/">Login\Join</Link></li>
        <li><Link to="/chat">Chat</Link></li>
      <Router>
